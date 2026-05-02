9e2cac187035376035f3b63a741e77d5f08c8099
Albert Abreu <elperiones4@gmail.com>
2026-05-02 06:46:54 -0400

    Add files via upload

--- Carrito de compras.py
+++ Carrito de compras.py
@@ -24,24 +24,43 @@ while continuar:
         precio = float(input(f"Precio de {nombre}: "))
         carrito.append(nombre)
         precios.append(precio)
-        print(f"{nombre} añadido con exito!")
+        print(f"{nombre} añadido con exito! \n")
 
     elif opcion == "2":
+        total = 0
         if len(carrito) == 0:
-            print("\n El carrito esta vacio.")
+            print("\n El carrito esta vacio. \n")
         else:
             print("\n--- Tu Lista de Compras ---")
-            total = 0
             for i in range(len(carrito)):
                 print(f"{i+1}. {carrito[i]} - ${precios[i]}")
                 total += precios[i]
-
-        print(f"TOTAL A PAGAR: ${total}")
-        print("---------------------------")
+                print(f"TOTAL A PAGAR: ${total} \n")
+                print("---------------------------")
 
     elif opcion == "3":
-        print("Gracias por su compra. Vuelva pronto!")
+        if len(carrito) == 0:
+            print("\n No hay nada que eliminar \n")
+        else:
+            for i in range(len(carrito)):
+                print(f"{i+1}. {carrito[i]}")
+            
+            indice = int(input("Introduce el número del producto a eliminar: ")) - 1
+            
+            if 0 <= indice < len(carrito):
+                eliminado = carrito.pop(indice)
+                precios.pop(indice)
+                print(f"Has eliminado: {eliminado} \n")
+            else:
+                print("Número no válido.")
+
+    elif opcion == "4":
+        total = sum(precios)
+        print(f"\n EL MONTO TOTAL ES: ${total} \n")
+
+    elif opcion == "5":
+        print("Gracias por su compra. ¡Vuelva pronto!")
         continuar = False
 
     else:
-        print("Opcion no valida, intente de nuevo.")
+        print("Opción no válida, intente de nuevo.")
